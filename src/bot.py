b87c8b08a8aac31b76b3c93e4974888fe8611fa4
zymsbgt <91821082+zymsbgt@users.noreply.github.com>
2023-03-13 14:00:38 +0800

    Make reset command work

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import sys
 from discord import app_commands
 from src import responses
 from src import log
@@ -7,7 +8,7 @@ from src import log
 logger = log.setup_logger(__name__)
 
 isPrivate = False
-isReplyAll = True
+isReplyAll = "True"
 
 class aclient(discord.Client):
     def __init__(self) -> None:
@@ -20,8 +21,7 @@ class aclient(discord.Client):
 
 async def send_message(message, user_message):
     global isReplyAll
-    author = message.author.id
-    if not isReplyAll:
+    if isReplyAll == "False":
         author = message.user.id
         await message.response.defer(ephemeral=isPrivate)
     else:
@@ -42,12 +42,11 @@ async def send_message(message, user_message):
 
                 for i in range(len(parts)):
                     if i%2 == 0: # indices that are even are not code blocks
-                        if isReplyAll:
+                        if isReplyAll == "True":
                             await message.channel.send(parts[i])
                         else:
                             await message.followup.send(parts[i])
 
-                    # Send the code block in a seperate message
                     else: # Odd-numbered parts are code blocks
                         code_block = parts[i].split("\n")
                         formatted_code_block = ""
@@ -63,31 +62,29 @@ async def send_message(message, user_message):
                             code_block_chunks = [formatted_code_block[i:i+char_limit]
                                                  for i in range(0, len(formatted_code_block), char_limit)]
                             for chunk in code_block_chunks:
-                                if isReplyAll:
-                                    await message.channel.send("```" + chunk + "```")
+                                if isReplyAll == "True":
+                                    await message.channel.send(f"```{chunk}```")
                                 else:
-                                    await message.followup.send("```" + chunk + "```")
+                                    await message.followup.send(f"```{chunk}```")
+                        elif isReplyAll == "True":
+                            await message.channel.send(f"```{formatted_code_block}```")
                         else:
-                            if isReplyAll:
-                                await message.channel.send("```" + formatted_code_block + "```")
-                            else:
-                                await message.followup.send("```" + formatted_code_block + "```")
+                            await message.followup.send(f"```{formatted_code_block}```")
 
             else:
                 response_chunks = [response[i:i+char_limit]
                                    for i in range(0, len(response), char_limit)]
                 for chunk in response_chunks:
-                    if isReplyAll:
+                    if isReplyAll == "True":
                         await message.channel.send(chunk)
                     else:
                         await message.followup.send(chunk)
+        elif isReplyAll == "True":
+            await message.channel.send(response)
         else:
-            if isReplyAll:
-                await message.channel.send(response)
-            else:
-                await message.followup.send(response)
+            await message.followup.send(response)
     except Exception as e:
-        if isReplyAll:
+        if isReplyAll == "True":
             await message.channel.send("> **Error: Something went wrong, please try again later!**")
         else:
             await message.followup.send("> **Error: Something went wrong, please try again later!**")
@@ -97,13 +94,13 @@ async def send_message(message, user_message):
 async def send_start_prompt(client):
     import os.path
 
-    config_dir = os.path.abspath(__file__ + "/../../")
+    config_dir = os.path.abspath(f"{__file__}/../../")
     prompt_name = 'starting-prompt.txt'
     prompt_path = os.path.join(config_dir, prompt_name)
     discord_channel_id = os.getenv("DISCORD_CHANNEL_ID")
     try:
         if os.path.isfile(prompt_path) and os.path.getsize(prompt_path) > 0:
-            with open(prompt_path, "r") as f:
+            with open(prompt_path, "r", encoding="utf-8") as f:
                 prompt = f.read()
                 if (discord_channel_id):
                     logger.info(f"Send starting prompt with size {len(prompt)}")
@@ -135,13 +132,13 @@ def run_discord_bot():
 
     @client.tree.command(name="chat", description="Have a chat with ChatGPT")
     async def chat(interaction: discord.Interaction, *, message: str):
-        global isReplyAll
-        # if isReplyAll:
-        #     await interaction.response.defer(ephemeral=False)
-        #     await interaction.followup.send(
-        #         "> **Warn: You already on replyAll mode. If you want to use slash command, switch to normal mode, use `/replyall` again**")
-        #     logger.warning("\x1b[31mYou already on replyAll mode, can't use slash command!\x1b[0m")
-        #     return
+        isReplyAll =  os.getenv("REPLYING_ALL")
+        if isReplyAll == "True":
+            await interaction.response.defer(ephemeral=False)
+            await interaction.followup.send(
+                "> **Warn: You already on replyAll mode. If you want to use slash command, switch to normal mode, use `/replyall` again**")
+            logger.warning("\x1b[31mYou already on replyAll mode, can't use slash command!\x1b[0m")
+            return
         if interaction.user == client.user:
             return
         username = str(interaction.user)
@@ -181,17 +178,20 @@ def run_discord_bot():
 
     # @client.tree.command(name="replyall", description="Toggle replyAll access")
     # async def replyall(interaction: discord.Interaction):
-    #     global isReplyAll
+    #     isReplyAll =  os.getenv("REPLYING_ALL")
+    #     os.environ["REPLYING_ALL_DISCORD_CHANNEL_ID"] = str(interaction.channel_id)
     #     await interaction.response.defer(ephemeral=False)
-    #     if isReplyAll:
+    #     if isReplyAll == "True":
+    #         os.environ["REPLYING_ALL"] = "False"
     #         await interaction.followup.send(
     #             "> **Info: The bot will only response to the slash command `/chat` next. If you want to switch back to replyAll mode, use `/replyAll` again.**")
     #         logger.warning("\x1b[31mSwitch to normal mode\x1b[0m")
-    #     else:
+    #     elif isReplyAll == "False":
+    #         os.environ["REPLYING_ALL"] = "True"
     #         await interaction.followup.send(
-    #             "> **Info: Next, the bot will response to all message in the server. If you want to switch back to normal mode, use `/replyAll` again.**")
+    #             "> **Info: Next, the bot will response to all message in this channel only.If you want to switch back to normal mode, use `/replyAll` again.**")
     #         logger.warning("\x1b[31mSwitch to replyAll mode\x1b[0m")
-    #     isReplyAll = not isReplyAll
+        
 
     # @client.tree.command(name="chat-model", description="Switch different chat model")
     # @app_commands.choices(choices=[
@@ -201,19 +201,27 @@ def run_discord_bot():
     # async def chat_model(interaction: discord.Interaction, choices: app_commands.Choice[str]):
     #     await interaction.response.defer(ephemeral=False)
     #     if choices.value == "OFFICIAL":
+    #         responses.chatbot = responses.get_chatbot_model("OFFICIAL")
     #         os.environ["CHAT_MODEL"] = "OFFICIAL"
     #         await interaction.followup.send(
     #             "> **Info: You are now in Official GPT-3.5 model.**\n> You need to set your `OPENAI_API_KEY` in `env` file.")
     #         logger.warning("\x1b[31mSwitch to OFFICIAL chat model\x1b[0m")
-    #     elif choices.value == "UNOFFCIAL":
+    #     elif choices.value == "UNOFFICIAL":
+    #         responses.chatbot = responses.get_chatbot_model("UNOFFICIAL")
     #         os.environ["CHAT_MODEL"] = "UNOFFICIAL"
     #         await interaction.followup.send(
     #             "> **Info: You are now in Website ChatGPT model.**\n> You need to set your `SESSION_TOKEN` or `OPENAI_EMAIL` and `OPENAI_PASSWORD` in `env` file.")
     #         logger.warning("\x1b[31mSwitch to UNOFFICIAL(Website) chat model\x1b[0m")
-    
+            
     @client.tree.command(name="reset", description="Complete reset ChatGPT conversation history")
     async def reset(interaction: discord.Interaction):
-        responses.chatbot.reset_chat()
+        await interaction.channel.send("Reboot signal sent")
+        sys.exit()
+        chat_model = os.getenv("CHAT_MODEL")
+        if chat_model == "OFFICIAL":
+            responses.chatbot.reset()
+        elif chat_model == "UNOFFICIAL":
+            responses.chatbot.reset_chat()
         await interaction.response.defer(ephemeral=False)
         await interaction.followup.send("> **Info: I have forgotten everything.**")
         logger.warning(
@@ -228,15 +236,16 @@ def run_discord_bot():
         - `/public` ChatGPT switch to public mode
         - `/replyall` ChatGPT switch between replyall mode and default mode
         - `/reset` Clear ChatGPT conversation history\n
-        For complete documentation, please visit https://github.com/Zero6992/chatGPT-discord-bot""")
+        For complete documentation, please visit <https://github.com/Zero6992/chatGPT-discord-bot>""")
         logger.info(
             "\x1b[31mSomeone need help!\x1b[0m")
 
     @client.event
     async def on_message(message):
-        if message.author == client.user:
-            return
-        if (isReplyAll):
+        global isReplyAll
+        if isReplyAll == "True":
+            if message.author == client.user:
+                return
             if client.user in message.mentions:
                 if ('instagram.com/p' in message.content) or ('instagram.com/reel' in message.content):
                     return
